512d5cb0c13cdcf0c3fa37a1e37bda20ab5053ec
1024Adam <avalancheaj10@gmail.com>
2015-11-22 10:28:36 -0500

    More added to GUI; fixed intentation issues in code

--- src/FileCompScreen.java
+++ src/FileCompScreen.java
@@ -15,66 +15,76 @@ import java.util.*;
 
 public class FileCompScreen extends JFrame
 {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public static final int WIDTH = 600;
+    public static final int WIDTH = 600;
     public static final int HEIGHT = 500;
     
     private static JPanel mainPanel = new JPanel();
     private static JPanel contentsPanel = new JPanel();
-    private static JPanel compPanel = new JPanel();
-    private static JPanel decompPanel = new JPanel();
-	
-	public FileCompScreen()
-	{
-		super();
-		setSize(WIDTH, HEIGHT);
-		setTitle("File Compressor");
-	
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    private static JPanel submitPanel = new JPanel();
+
+    public FileCompScreen()
+    {
+        super();
+        setSize(WIDTH, HEIGHT);
+        setTitle("File Compressor");
+
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         //addWindowListener(); // need to add a WindowListener
-		
-		// Create Menu Bar 
-		JMenuBar bar = new JMenuBar();
-		JMenu file = new JMenu("File");
-		
-		JMenuItem quit = new JMenuItem("Quit");
-		//quit.addActionListener(new QuitListener());
-		file.add(quit);
-		bar.add(file);
-		
-		setJMenuBar(bar);
-		
-		// Setup main panel
-		mainPanel.setBackground(Color.WHITE);
-		mainPanel.setLayout(new BorderLayout());
-		
-		JToggleButton toggle = new JToggleButton("Compress", false);
-		toggle.addChangeListener(new ChangeListener()
-		{
-			@Override
-			public void stateChanged(ChangeEvent event) 
-			{
-				if (!toggle.isSelected())
-				{
-					toggle.setText("Compress");
-				} 
-				else 
-				{
-					toggle.setText("Decompress");
-				}
-	        }
-		});
-		mainPanel.add(toggle, BorderLayout.NORTH);
-		
-		// Setup contents panel
-		contentsPanel.setBackground(Color.WHITE);
-		contentsPanel.setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
-		
-		
-		
-		add(mainPanel);
-		
-		
-	}
+
+        // Create Menu Bar 
+        JMenuBar bar = new JMenuBar();
+        JMenu file = new JMenu("File");
+
+        JMenuItem quit = new JMenuItem("Quit");
+        //quit.addActionListener(new QuitListener()); // need to add a quit listener
+        file.add(quit);
+        bar.add(file);
+        setJMenuBar(bar);
+
+        // Setup main panel
+        mainPanel.setBackground(Color.WHITE);
+        mainPanel.setLayout(new BorderLayout());
+
+        JToggleButton toggle = new JToggleButton("Compress", false);
+        toggle.addChangeListener(new ChangeListener()
+        {
+            @Override
+            public void stateChanged(ChangeEvent event) 
+            {
+                if (!toggle.isSelected())
+                {
+                    toggle.setText("Compress");
+                } 
+                else 
+                {
+                    toggle.setText("Decompress");
+                }
+            }
+        });
+        JPanel topPanel = new JPanel();
+        topPanel.setBackground(Color.WHITE);
+        topPanel.add(toggle);
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+
+        // Setup contents panel
+        contentsPanel.setBackground(Color.WHITE);
+        contentsPanel.setLayout(new GridLayout(0, 2));
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setBackground(Color.WHITE);
+        JButton searchButton = new JButton("Browse...");
+        centerPanel.add(searchButton);
+        contentsPanel.add(centerPanel);
+
+        JPanel centerPanel2 = new JPanel();
+        centerPanel2.setBackground(Color.WHITE);
+        JLabel fileLabel = new JLabel("hello");
+        centerPanel2.add(fileLabel); 
+        contentsPanel.add(centerPanel2);
+
+        mainPanel.add(contentsPanel, BorderLayout.CENTER);
+        add(mainPanel);
+    }
 }
--- src/FileCompression.java
+++ src/FileCompression.java
@@ -1,11 +1,9 @@
 
-public class FileCompression {
-
-	public static void main(String[] args) 
-	{
-		FileCompScreen screen = new FileCompScreen();
-		
-		screen.setVisible(true);
-	}
-
+public class FileCompression 
+{
+    public static void main(String[] args) 
+    {
+        FileCompScreen screen = new FileCompScreen();
+        screen.setVisible(true);
+    }
 }
