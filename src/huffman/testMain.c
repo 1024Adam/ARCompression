39ca4faebced7a204ecab50e3e87e3471db4a3a7
1024Adam <avalancheaj10@gmail.com>
2016-03-06 15:37:10 -0500

    Added testing files; Modified C binary arguments/requirements

--- src/huffman/testMain.c
+++ src/huffman/testMain.c
@@ -2,16 +2,34 @@
 
 /*
  * Adam Reid
- * December 30, 2015
+ * March 6, 2016
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include "huffman.h"
 
 int main(int argc, char ** argv)
 {
-    encode(argv[1]);
-    /*decode(argv[1]);*/
+    if((argc == 3) && (strcmp(argv[1], "-d") == 0))
+    {
+        decode(argv[2]);
+    }
+    else if(((argc == 3) && (strcmp(argv[1], "-c") == 0)) || (argc == 2))
+    {
+        if(argc == 2)
+        {
+            encode(argv[1]);
+        }
+        else
+        {
+            encode(argv[2]);
+        }
+    }
+    else
+    {
+        printf("Error: invalid arguments\n");
+    }
     return(0);
 }
